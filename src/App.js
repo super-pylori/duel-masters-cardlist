e219026b4d3af8304c17103e3a8ee4eb04d49df3
super-pylori <super.pylori6@gmail.com>
2023-03-02 18:57:28 +0900

    スマホで2列表示にする

--- src/App.js
+++ src/App.js
@@ -32,27 +32,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ul class="photo-list">
+        <div className="photo-list">
           {urls.map((url) => {
             return (
-              <li>
-                <img src={url} className="App-logo"/>
-              </li>
+                <img src={url} className="photo-list-img"/>
             );
           })}
-        </ul>
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </div>
       </header>
     </div>
   );
